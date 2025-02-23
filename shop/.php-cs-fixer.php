ce5c723cc2c7e1be893e3c3e3686a24f087432b4
Soft Well House <bialonj@gmail.com>
2025-02-23 20:45:52 +0100

    CI actions

---
+++ shop/.php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(['src', 'tests']) // Określa katalogi do sprawdzania
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true, // Stosuje standard PSR-12
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
