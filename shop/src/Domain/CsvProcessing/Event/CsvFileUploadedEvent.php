5cac3803f269819c383d6731d77324ceb52b3779
Soft Well House <bialonj@gmail.com>
2025-02-25 14:41:41 +0100

    csv file event

---
+++ shop/src/Domain/CsvProcessing/Event/CsvFileUploadedEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\CsvProcessing\Event;
+
+
+class CsvFileUploadedEvent
+{
+    private string $filename;
+
+    public function __construct(string $filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+}
