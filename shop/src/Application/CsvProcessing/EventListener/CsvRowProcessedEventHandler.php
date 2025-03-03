958a55b2c05ad8071e75bda90efd6b3872f484d2
Soft Well House <bialonj@gmail.com>
2025-03-03 09:02:27 +0100

    handler for row

--- shop/src/Application/CsvProcessing/EventListener/CsvRowProcessedEventHandler.php
+++ shop/src/Application/CsvProcessing/EventListener/CsvRowProcessedEventHandler.php
@@ -2,24 +2,42 @@
 
 namespace App\Application\CsvProcessing\EventListener;
 
+use App\Domain\CsvProcessing\Enum\CsvFileUploadStatusEnum;
 use App\Domain\CsvProcessing\Event\CsvRowProcessedEvent;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class CsvRowProcessedEventHandler
 {
-    public function __construct(private readonly LoggerInterface $logger)
+
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private CacheInterface           $cache,
+        private readonly RequestStack    $requestStack,
+    )
     {
     }
 
     #[AsEventListener(event: CsvRowProcessedEvent::class)]
     public function onCsvRowProcessed(CsvRowProcessedEvent $event): void
     {
-        $this->logger->info("Obsługa wiersza CSV z pliku: {$event->getFilename()}, Linia: {$event->getRow()}");
+        $this->cache->get('csv_status_' . $event->getUuid(), [
+            'progress' => (int)(($event->getRow() / $event->getTotalRow()) * 100),
+            'status' => CsvFileUploadStatusEnum::PENDING,
+        ]);
+
+        $message = "Obsługa wiersza CSV z: {$event->getUuid()}, Linia: {$event->getRow()}";
+        $this->logger->info($message);
 
         $data = $event->getData();
 
         // Tutaj możemy zwalidować dane i zapisywać dane do bazy
+        $session = $this->requestStack->getSession();
+        $session->set("file_progress_{$event->getUuid()}", (int)(($event->getRow() / $event->getTotalRow()) * 100));
+
+        $this->cache->get('csv_' . $event->getUuid(), ['csv' => $data]);
 
         $this->logger->info("Przetworzono linię {$event->getRow()}");
     }
