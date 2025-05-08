ca0c8cd8d8ddd1e3c874edcd84706a0f48fd7f53
Soft Well House <bialonj@gmail.com>
2025-05-08 20:22:38 +0200

    Client

---
+++ shop/src/Application/Client/UI/Http/Controller/ClientController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Application\Client\UI\Http\Controller;
+
+use App\Domain\Client\Enum\TypeClientEnum;
+use App\Validator\Constraints\CsvFile;
+use App\Validator\Constraints\CsvFileEmpty;
+use PHPStan\Type\StringType;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\TypeInfo\Type\EnumType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[Route('/client')]
+class ClientController extends AbstractController
+{
+    public function __construct(
+        private readonly RequestStack    $requestStack,
+    )
+    {
+    }
+
+    #[Route('/', name: 'client_index')]
+    public function index(): Response
+    {
+
+        return $this->render('@client/index.html.twig', [
+            'variable' => 'Witaj w Symfony!',
+        ]);
+    }
+
+    #[Route('/create', name: 'client_create')]
+    public function create(): Response
+    {
+
+        return $this->render('@client/create_form.html.twig', [
+            'variable' => 'Witaj w Symfony!',
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'client_edit')]
+    public function edit(int $id): Response
+    {
+
+        return $this->render('@client/create_form.html.twig', [
+            'variable' => 'Witaj w Symfony!',
+        ]);
+    }
+
+
+    private function form()
+    {
+        return $this->createFormBuilder()
+            ->add('client_type', EnumType::class, [
+                'class' => TypeClientEnum::class,
+                'label' => 'Typ klienta',
+                'required' => true,
+                'constraints' => [],
+            ])
+            ->add('company_name', StringType::class, [
+                'label' => 'Nazwa firmy',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new CsvFile(),
+                    new CsvFileEmpty(),
+                ],
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Prześlij'])
+            ->getForm();
+    }
+}
+
